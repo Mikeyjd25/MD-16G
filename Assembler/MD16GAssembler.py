25b296873dce8a4e542a82073ef3a620da61613f
Mikeyjd25 <mikeydthemonkey@gmail.com>
2017-12-15 07:44:36 +0000

    Copied in some files from previous project.

---
+++ Assembler/MD16GAssembler.py
@@ -0,0 +1,259 @@
+import binascii
+import time
+
+start_time = int(round(time.time() * 1000))
+
+in_file = "D:/ROMs/MD/MD-16X/1.asm"
+out_file = "D:/ROMs/MD/MD-16X/1.MDR"
+
+
+def reg(reg_id):
+    if reg_id == "GP0":
+        return "00000"
+    elif reg_id == "GP1":
+        return "00001"
+    elif reg_id == "GP2":
+        return "00010"
+    elif reg_id == "GP3":
+        return "00011"
+    elif reg_id == "GP4":
+        return "00100"
+    elif reg_id == "GP5":
+        return "00101"
+    elif reg_id == "GP6":
+        return "00110"
+    elif reg_id == "GP7":
+        return "00111"
+    elif reg_id == "ALU":
+        return "01000"
+    elif reg_id == "ALF":
+        return "01001"
+    elif reg_id == "AMC":
+        return "01010"
+    elif reg_id == "FLG":
+        return "01011"
+    elif reg_id == "LMT":
+        return "01100"
+    elif reg_id == "LFM":
+        return "01101"
+    elif reg_id == "CSP":
+        return "01110"
+    elif reg_id == "LSP":
+        return "01111"
+    elif reg_id == "LT0":
+        return "10000"
+    elif reg_id == "LT1":
+        return "10001"
+    elif reg_id == "LT2":
+        return "10010"
+    elif reg_id == "LT3":
+        return "10011"
+    elif reg_id == "LT4":
+        return "10100"
+    elif reg_id == "LT5":
+        return "10101"
+    elif reg_id == "LT6":
+        return "10110"
+    elif reg_id == "LT7":
+        return "10111"
+    elif reg_id == "LT8":
+        return "11000"
+    elif reg_id == "LT9":
+        return "11001"
+    elif reg_id == "LTA":
+        return "11010"
+    elif reg_id == "LTB":
+        return "11011"
+    elif reg_id == "LTC":
+        return "11100"
+    elif reg_id == "LTD":
+        return "11101"
+    elif reg_id == "LTE":
+        return "11110"
+    elif reg_id == "LTF":
+        return "11111"
+    else:
+        raise Exception('Unrecognised register: "' + reg_id + '" [' + str(line) + ']')
+
+
+bin_count = 0
+goto_list = {}
+
+
+def add_hex(hex_d, reg_ax="00000", reg_bx="00000", data="00000000"):
+    global bin_count
+    global hex_data
+    if not ((len(hex_d) == 6) & (len(reg_ax) == 5) & (len(reg_bx) == 5) & (len(data) == 8)):
+        raise Exception('Something went wrong... Sorry? "' + com + '"\n[' + str(line) + '] ' + x + '\n' +
+                        hex_d+' '+reg_ax+' '+reg_bx+' '+data)
+    if data == "00000000":
+        hex_data.append(hex_d+reg_ax+reg_bx)
+    else:
+        hex_data.append(hex_d + "00" + data)
+    bin_count += 1
+
+
+def force_add_hex(hex_d):
+    global bin_count
+    global hex_data
+    hex_data.append(hex_d)
+    bin_count += 1
+
+
+def add_jump(jump_hex, use_reg, goto_id):
+    global goto_list
+    if goto_id not in goto_list:
+        goto_list[goto_id] = '-1'
+    add_hex("000001", use_reg)
+    force_add_hex("GOTO-"+goto_id)
+    add_hex(jump_hex, use_reg)
+
+
+def add_goto(goto_id):
+    global bin_count
+    global goto_list
+    goto_list[goto_id] = format(bin_count, '016b')
+
+# "%0.4x" % number
+with open(in_file) as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+hex_data = []
+line = 0
+
+
+for x in content:
+    line += 1
+    x = x.split('#')[0].strip()
+    if len(x) == 0:
+        continue
+    cdata = x.split()
+    if len(cdata[0].strip()) > 0:
+        com = cdata[0].strip().upper()
+        reg_a = ""
+        reg_b = ""
+        if len(cdata) > 1:
+            reg_a = cdata[1].strip().upper()
+        if len(cdata) > 2:
+            reg_b = cdata[2].strip().upper()
+        if com == "NOP":  # No op
+            add_hex("000000")
+        elif com == "LDW":  # Load Word
+            add_hex("000001", reg(reg_a))
+            temp_bin = format(int(reg_b, 16), '016b')
+            add_hex(temp_bin[0:6], temp_bin[6:11], temp_bin[11:16])
+        elif com == "LDL":  # Load half word, lower
+            add_hex("000010", data=format(int(reg_a, 16), '08b'))
+        elif com == "LDU":  # Load half word, upper
+            add_hex("000011", data=format(int(reg_a, 16), '08b'))
+        elif com == "EXO":  # Execute ROM
+            add_hex("000100")
+        elif com == "EXA":  # Execute RAM
+            add_hex("000101")
+        elif com == "JMP":  # Jump
+            add_jump("001000", reg(reg_a), reg_b)
+        elif com == "JLZ":  # Jump if less than zero
+            add_jump("001001", reg(reg_a), reg_b)
+        elif com == "JGZ":  # Jump if greater than zero
+            add_jump("001010", reg(reg_a), reg_b)
+        elif com == "JEZ":  # Jump if zero
+            add_jump("001011", reg(reg_a), reg_b)
+        elif com == "JNZ":  # Jump if not zero
+            add_jump("001100", reg(reg_a), reg_b)
+        elif com == "JCS":  # Jump if carry set
+            add_jump("001101", reg(reg_a), reg_b)
+        elif com == "STR":  # Store
+            add_hex("001110", reg(reg_a), reg(reg_b))
+        elif com == "RET":  # Retrieve
+            add_hex("001111", reg(reg_a), reg(reg_b))
+
+        elif com == "ADD":  # Add
+            add_hex("010000", reg(reg_a), reg(reg_b))
+        elif com == "SUB":  # Subtract
+            add_hex("010001", reg(reg_a), reg(reg_b))
+        elif com == "MUL":  # Multiply
+            add_hex("010010", reg(reg_a), reg(reg_b))
+        elif com == "CPY":  # Copy
+            add_hex("010011", reg(reg_a), reg(reg_b))
+        elif com == "SHL":  # Shift Left
+            add_hex("010100", reg(reg_a))
+        elif com == "SHR":  # Shift Right
+            add_hex("010101", reg(reg_a))
+        elif com == "ROL":  # Rotate Left
+            add_hex("010110", reg(reg_a))
+        elif com == "ROR":  # Rotate Right
+            add_hex("010111", reg(reg_a))
+        elif com == "AND":  # AND
+            add_hex("011000", reg(reg_a), reg(reg_b))
+        elif com == "NAN":  # NAND
+            add_hex("011001", reg(reg_a), reg(reg_b))
+        elif com == "ORR":  # OR
+            add_hex("011010", reg(reg_a), reg(reg_b))
+        elif com == "NOR":  # NOR
+            add_hex("011011", reg(reg_a), reg(reg_b))
+        elif com == "XOR":  # XOR
+            add_hex("011100", reg(reg_a), reg(reg_b))
+        elif com == "XNR":  # XNOR
+            add_hex("011101", reg(reg_a), reg(reg_b))
+        elif com == "NOT":  # NOT
+            add_hex("011110", reg(reg_a))
+        elif com == "REV":  # Reverse
+            add_hex("011111", reg(reg_a))
+
+        elif com == "AWC":  # Add With Carry
+            add_hex("100000", reg(reg_a), reg(reg_b))
+        elif com == "SLC":  # Shift Left into carry
+            add_hex("100100", reg(reg_a))
+        elif com == "SRC":  # Shift right into carry
+            add_hex("100101", reg(reg_a))
+        elif com == "RLC":  # Rotate left though carry
+            add_hex("100110", reg(reg_a))
+        elif com == "RRC":  # Rotate right through carry
+            add_hex("100111", reg(reg_a))
+        elif com == "INC":  # increment
+            add_hex("101000", reg(reg_a))
+        elif com == "DEC":  # decrement
+            add_hex("101001", reg(reg_a))
+        elif com == "ELB":  # extract lower byte
+            add_hex("101100", reg(reg_a))
+        elif com == "EUB":  # extract upper byte
+            add_hex("101101", reg(reg_a))
+        elif com == "WLB":  # write lower byte
+            add_hex("101110", reg(reg_a), reg(reg_b))
+        elif com == "WUB":  # write upper byte
+            add_hex("101111", reg(reg_a), reg(reg_b))
+
+        elif com == "RTX":  # Read UART
+            add_hex("110000", reg(reg_a))
+        elif com == "RDK":  # Read disk
+            add_hex("110001", reg(reg_a))
+        elif com == "WTX":  # Write UART
+            add_hex("110100", reg(reg_a))
+        elif com == "WDK":  # Write disk
+            add_hex("110101", reg(reg_a))
+
+        elif com == "DEF":  # Add jump location
+            add_goto(reg_a)
+        else:
+            raise Exception('Unrecognised command: "' + com + '"\n[' + str(line) + '] ' + x)
+
+program_len = len(hex_data)
+
+for key in goto_list:
+    if goto_list[key] == '-1':
+        raise Exception('Missing GOTO: "' + str(key) + '"')
+    else:
+        hex_data = [w.replace("GOTO-"+key, goto_list[key]) for w in hex_data]
+
+hex_data2 = []
+for each in hex_data:
+    hex_data2.append(("%0.4x" % int(each, 2)).upper())
+
+with open(out_file, 'wb') as f:
+    f.write(binascii.unhexlify(''.join(hex_data2)))
+
+
+print("Size: ["+str(program_len)+"/65536]")
+end_time = int(round(time.time() * 1000))
+
+print("Took: " + str(end_time-start_time) + "ms")
---
+++ Emulator/Emulator.pde
@@ -0,0 +1,262 @@
+int ROM[] = new int[65536]; //64KB of ROM
+int REG[] = new int[32];    //32 Registers
+int RAM[] = new int[65536]; //64KB of RAM
+int PROC = 0; //Program counter
+
+//---REGISTERS---
+public static final int REG_GP_0 = 0;
+public static final int REG_GP_1 = 1;
+public static final int REG_GP_2 = 2;
+public static final int REG_GP_3 = 3;
+public static final int REG_GP_4 = 4;
+public static final int REG_GP_5 = 5;
+public static final int REG_GP_6 = 6;
+public static final int REG_GP_7 = 7;
+public static final int REG_ALU_OUT = 8;
+public static final int REG_ALU_FLAGS = 9;
+public static final int REG_ALU_MULT_CARRY = 10;
+public static final int REG_FLAGS = 11;
+public static final int REG_LUT_MATCH_TABLE = 12;
+public static final int REG_LUT_FIRST_MATCH = 13;
+public static final int REG_CURRENT_STACK_POINTER = 14;
+public static final int REG_LAST_STACK_POINTER = 15;
+
+public static final int REG_LUT_0 = 16;
+public static final int REG_LUT_1 = 17;
+public static final int REG_LUT_2 = 18;
+public static final int REG_LUT_3 = 19;
+public static final int REG_LUT_4 = 20;
+public static final int REG_LUT_5 = 21;
+public static final int REG_LUT_6 = 22;
+public static final int REG_LUT_7 = 23;
+public static final int REG_LUT_8 = 24;
+public static final int REG_LUT_9 = 25;
+public static final int REG_LUT_10 = 26;
+public static final int REG_LUT_11 = 27;
+public static final int REG_LUT_12 = 28;
+public static final int REG_LUT_13 = 29;
+public static final int REG_LUT_14 = 30;
+public static final int REG_LUT_15 = 31;
+
+boolean exec_ram = false;
+void setup() {
+  size(1280, 960);
+  // Open a file and read its binary data
+  byte b[] = loadBytes("D:/ROMs/MD/MD-16X/1.MDR");
+  
+  // Print each value, from 0 to 255
+  for (int i = 0; i < b.length; i+=2) {
+    // bytes are from -128 to 127, this converts to 0 to 255
+    int a = ((b[i] & 0xff)<<8)|(b[i+1] & 0xff);
+    ROM[i/2] = a;
+  }
+}
+int delta = 0;
+int last = 0;
+int last2 = 0;
+int insrt_count = 0;
+void draw() {
+  delta = millis()-last;
+  if (delta>=1000){
+    surface.setTitle("PROC: " + Integer.toString(insrt_count/delta/1000) + "MHz");
+    insrt_count = 0;
+    last = millis();
+  }
+  last2=millis();
+  while(millis()-last2<20){
+    for(int i = 0; i<1000; i++){
+      insrt_count++;
+      int word = 0;
+      if(exec_ram) word = RAM[PROC]; else word = ROM[PROC];
+      int word2 = 0;
+      if(exec_ram) word2 = RAM[(PROC+1)%65536]; else word2 = ROM[(PROC+1)%65536];
+      int command = ((word >> 10) & 0x003F);
+      int regA = ((word >> 5) & 0x001F);
+      int regB = (word & 0x001F);
+      int data = (word & 0x00FF);
+      switch (command) {
+        case 0:  //00 No op
+                 break;
+        case 1:  //01 Load Word
+          regw(regA,word2);
+          insrt_count++;
+                 break;
+        case 2:  //02 Load half word, lower
+          alu_out(data);
+                 break;
+        case 3:  //03 Load half word, upper
+          alu_out(data<<8);
+                 break;
+        case 4:  //04 Execute ROM
+          exec_ram = false;
+          PROC = 0;
+                 break;
+        case 5:  //05 Execute RAM
+          exec_ram = true;
+          PROC = 0;
+                 break;
+        case 6:  //06 
+                 break;
+        case 7:  //07 
+                 break;
+        case 8:  //08 Jump
+          PROC = regr(regA)-1;
+                 break;
+        case 9:  //09 Jump if less than zero
+          if((REG[REG_ALU_FLAGS]&0x0002)>0) PROC = regr(regA)-1;
+                 break;
+        case 10: //0A Jump if greater than zero
+          if((REG[REG_ALU_FLAGS]&0x0004)>0) PROC = regr(regA)-1;
+                 break;
+        case 11: //0B Jump if zero
+          if((REG[REG_ALU_FLAGS]&0x0008)>0) PROC = regr(regA)-1;
+                 break;
+        case 12: //0C Jump if not zero
+          if((REG[REG_ALU_FLAGS]&0x0010)>0) PROC = regr(regA)-1;
+                 break;
+        case 13: //0D Jump if carry set
+          if((REG[REG_ALU_FLAGS]&0x0001)>0) PROC = regr(regA)-1;
+                 break;
+        case 14: //0E Store
+          RAM[regr(regA)] = regr(regB);
+                 break;
+        case 15: //0F Retrieve
+          regw(regB, RAM[regr(regA)]);
+                 break;
+        case 16: //10 Add
+          alu_out((regr(regA)+regr(regB))&0xFFFF);
+                 break;
+        case 17: //11 Subtract
+          alu_out((regr(regA)-regr(regB))&0xFFFF);
+                 break;
+        case 18: //12 Multiply
+          alu_out((regr(regA)*regr(regB))&0xFFFF);
+                 break;
+        case 19: //13 Copy
+          regw(regB,regr(regA));
+                 break;
+        case 20: //14 Shift Left
+          alu_out((regr(regA)<<1)&0xFFFF);
+                 break;
+        case 21: //15 Shift Right
+          alu_out((regr(regA)>>1)&0xFFFF);
+                 break;
+        case 22: //16 Rotate Left
+                 break;
+        case 23: //17 Rotate Right
+                 break;
+        case 24: //18 AND
+          alu_out((regr(regA)&regr(regB))&0xFFFF);
+                 break;
+        case 25: //19 NAND
+          alu_out((~(regr(regA)&regr(regB)))&0xFFFF);
+                 break;
+        case 26: //1A OR
+          alu_out((regr(regA)|regr(regB))&0xFFFF);
+                 break;
+        case 27: //1B NOR
+          alu_out((~(regr(regA)|regr(regB)))&0xFFFF);
+                 break;
+        case 28: //1C XOR
+          alu_out((regr(regA)^regr(regB))&0xFFFF);
+                 break;
+        case 29: //1D XNOR
+          alu_out((~(regr(regA)^regr(regB)))&0xFFFF);
+                 break;
+        case 30: //1E NOT
+          alu_out((~regr(regA))&0xFFFF);
+                 break;
+        case 31: //1F Reverse
+          alu_out((Integer.reverse(regr(regA))>>16)&0xFFFF);
+                 break;
+        case 32: //20 Add With Carry
+                 break;
+        case 33: //21 
+                 break;
+        case 34: //22 
+                 break;
+        case 35: //23 
+                 break;
+        case 36: //24 Shift Left into carry
+                 break;
+        case 37: //25 Shift right into carry
+                 break;
+        case 38: //26 Rotate left though carry
+                 break;
+        case 39: //27 Rotate right through carry
+                 break;
+        case 40: //28 increment
+          regw(regA,(regr(regA)+1)&0xFFFF);
+                 break;
+        case 41: //29 decrement
+          regw(regA,(regr(regA)-1)&0xFFFF);
+                 break;
+        case 42: //2A 
+                 break;
+        case 43: //2B 
+                 break;
+        case 44: //2C extract lower byte
+                 break;
+        case 45: //2D extract upper byte
+                 break;
+        case 46: //2E write lower byte
+                 break;
+        case 47: //2F write upper byte
+                 break;
+        case 48: //30 Read UART
+                 break;
+        case 49: //31 Read disk
+                 break;
+        case 50: //32 
+                 break;
+        case 51: //33 
+                 break;
+        case 52: //34 Write UART
+          print((char) (regr(regA)&0xFF));
+                 break;
+        case 53: //35 Write disk
+                 break;
+        case 54: //36 
+                 break;
+        case 55: //37 
+                 break;
+        case 56: //38 
+                 break;
+        case 57: //39 
+                 break;
+        case 58: //3A 
+                 break;
+        case 59: //3B 
+                 break;
+        case 60: //3C 
+                 break;
+        case 61: //3D 
+                 break;
+        case 62: //3E 
+                 break;
+        case 63: //3F 
+                 break;
+      }
+      PROC++;
+      PROC %= 65536;
+    }
+  }
+}
+void alu_out(int a){alu_out(a, false);}
+void alu_out(int a, boolean carry){
+  REG[REG_ALU_OUT] = a;
+  int flags_temp = 0;
+  if(carry) flags_temp |= 0x0001;
+  if((a&0x8000)!=0) flags_temp |= 0x0002;
+  if((a&0x8000)==0&&a!=0) flags_temp |= 0x0004;
+  if(a==0) flags_temp |= 0x0008;
+  if(a!=0) flags_temp |= 0x0010;
+  REG[REG_ALU_FLAGS] = flags_temp;
+}
+int regr(int a){
+  if(a!=11) if(a>=0&&a<32)return REG[a];
+  return 0;
+}
+void regw(int a, int b){
+  if(a!=11) if(a>=0&&a<32) REG[a]=b;
+}
