2d8c3ee3bb59f5945a1db42a18e059784e869e34
Mikeyjd25 <mikeyjd25@yahoo.co.uk>
2017-12-20 22:20:37 +0000

    Added HelloWorld

---
+++ TestCode/HelloWorld.asm
@@ -0,0 +1,32 @@
+############################
+#Just a simple Hello World!#
+#By Mikeyjd25              #
+############################
+
+LDW GP0 48 #H
+CPY GP0 CM0
+LDW GP0 65 #e
+CPY GP0 CM0
+LDW GP0 6C #l
+CPY GP0 CM0
+CPY GP0 CM0
+LDW GP0 6F #o
+CPY GP0 CM0
+LDW GP0 20 #' '
+CPY GP0 CM0
+LDW GP0 57 #W
+CPY GP0 CM0
+LDW GP0 6F #o
+CPY GP0 CM0
+LDW GP0 72 #r
+CPY GP0 CM0
+LDW GP0 6C #l
+CPY GP0 CM0
+LDW GP0 64 #d
+CPY GP0 CM0
+LDW GP0 21 #!
+CPY GP0 CM0
+
+
+DEF END #Loop forever
+JMP GP0 END
