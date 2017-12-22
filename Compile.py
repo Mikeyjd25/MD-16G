32bd67810be6f0dcecbe6cec63e49278f2e118b1
Mikeyjd25 <mikeyjd25@yahoo.co.uk>
2017-12-22 01:29:52 +0000

    Added Compile.py
    Added Compile.py
    Added '*.pyc' to .gitignore
    Added __init__.py to Assembler so that it can be used as a library.

---
+++ Compile.py
@@ -0,0 +1,9 @@
+#################################
+# Compilation automation script #
+# Made by Mikeyjd25             #
+# 2017                          #
+#################################
+
+from Assembler import MD16GAssembler
+
+MD16GAssembler.main("TestCode/HelloWorld.asm","TestCode/Bootloader.rom")
