c568f0fba274d245c3e09b269945d71ff8e955cc
Mikeyjd25 <michaeljduthie@gmail.com>
2017-12-28 15:58:46 +0000

    Updated Compile.py to run compiler

--- Compile.py
+++ Compile.py
@@ -4,6 +4,8 @@
 # 2017                          #
 #################################
 
-from Assembler import MD16GAssembler
+#from Assembler import MD16GAssembler
+from Compiler import Compiler
 
-MD16GAssembler.main("TestCode/HelloWorld.asm","TestCode/Bootloader.rom")
+#MD16GAssembler.main("TestCode/HelloWorld.asm", "TestCode/Bootloader.rom")
+Compiler.main("OS/MAIN.GYB", "TEMP/MAIN.ASM")
